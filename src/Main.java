efb845fb886ca3e78070a20b4c74fdee1d3a73f2
cornelgd <corneldx@gmail.com>
2020-11-06 16:56:42 +0200

    empty cell clearing works

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,7 @@ import javax.swing.table.DefaultTableModel;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.Arrays;
 import java.util.Random;
 
 public class Main {
@@ -14,6 +15,9 @@ public class Main {
         final int gridSize = 9;
         public int casute = gridSize * gridSize;
         public int[] bombeA = new int[bombe];
+        public boolean [][] check = new boolean[gridSize][gridSize];
+        public JFrame frameMeniu = new JFrame("Welcome");
+      public  JButton[][] buttons = new JButton[gridSize][gridSize];
 
         joc() {
 
@@ -22,14 +26,13 @@ public class Main {
             int height = gridSize * patratel + 120;
             int centerw = (width - (gridSize * patratel)) / 2 - 8;
             int centerh = (height - (gridSize * patratel)) / 2 - 20;
-            JFrame frameMeniu = new JFrame("Welcome");
             DefaultTableModel model = new DefaultTableModel() {
                 @Override
                 public boolean isCellEditable(int row, int column) {
                     return false;
                 }
             };
-            JTable desen = new JTable(model);
+           JTable desen = new JTable(model);
 
             desen.setFont(new Font("Arial", Font.BOLD, 17));
             frameMeniu.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -67,7 +70,6 @@ public class Main {
             }
 
 
-            JButton[][] buttons = new JButton[gridSize][gridSize];
             for (int i = 0; i < gridSize; i++) {
                 for (int j = 0; j < gridSize; j++) {
                     buttons[i][j] = new JButton();
@@ -146,6 +148,9 @@ public class Main {
                                 frameMeniu1.setVisible(true);
 
                             }
+
+                            findEmptyCells(desen,finalI,finalJ);
+
                             frameMeniu.getContentPane().remove(buttons[finalI][finalJ]);
 
                             frameMeniu.revalidate();
@@ -157,6 +162,53 @@ public class Main {
                 }
             }
         }
+
+
+
+
+
+        public void findEmptyCells(JTable desen, int i, int j) // this method is called when a cell is clicked, therefore all adjacent empty cells will be opened
+        {
+
+                if (i >= 0 && j >= 0 && i < gridSize && j < gridSize && check[i][j]==false) //ie the block actually exists on the grid
+                {
+                    System.out.println("valoarea la i " + i + " valoarea la j " + j + "=" + desen.getValueAt(j, i));
+                    if (desen.getValueAt(j, i) == null) //if cell is empty & not a mine & not flagged
+                    {
+                        check[i][j]=true;
+                        System.out.println("gol");
+                        int finalI = i;
+                        int finalJ = j;
+                       frameMeniu.getContentPane().remove(buttons[finalI][finalJ]);
+                       frameMeniu.revalidate();
+                        frameMeniu.repaint();
+                        findEmptyCells(desen, i - 1, j); //left
+                        findEmptyCells(desen, i + 1, j); //right
+                        findEmptyCells(desen, i, j + 1); //up
+                       findEmptyCells(desen, i, j - 1); //down
+                       // findEmptyCells(desen, i - 1, j + 1); //up-left
+                       // findEmptyCells(desen, i + 1, j + 1); //up-right
+                       // findEmptyCells(desen, i - 1, j - 1); //down-left
+                       // findEmptyCells(desen, i + 1, j - 1); //down-right
+
+                    } else if (desen.getValueAt(j, i) != null) {
+                        // /buttons[i][j].setIcon(new ImageIcon("buttonImages/but" + cells[i][j].getAdjMines() + ".png"));
+                        //  cells[i][j].setIsOpen(true); //for later, if we need to identify which cells are still unclicked
+
+                        return;
+                    }
+
+                } else {
+                    return;
+                }
+
+                    }
+
+
+
+
+
+
 
         private int[] generateBomb(int bombe) {
             Random rand = new Random();
